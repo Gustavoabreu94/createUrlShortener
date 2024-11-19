086a39e70f676962338fe9b14bccb846f9335624
Gustavo Abreu <you@example.com>
2024-11-19 11:30:23 -0300

    second class finished

---
+++ src/main/java/com/rocketseat/createUrlShortener/UrlData.java
@@ -0,0 +1,21 @@
+package com.rocketseat.createUrlShortener;
+
+
+//import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+//@AllArgsConstructor
+@Setter
+@Getter
+public class UrlData {
+
+    private String originalUrl;
+    private long expirationTime;
+
+    public UrlData(String originalUrl, long expirationTime) {
+        this.originalUrl = originalUrl;
+        this.expirationTime = expirationTime;
+    }
+
+}
